e19f70672698e52fb074cd6182b44f0a21a9bcab
Bhagyashri Babasaheb Fulari <bhagyashribfulari@gmail.com>
2025-09-24 22:54:07 +0530

    Add files via upload

---
+++ Pattern Printing/85_PatternPrintCharNTimesErr.c
@@ -0,0 +1,43 @@
+// Input : 4    #
+// Output : #   #   #   #
+
+
+#include<stdio.h>
+
+void Display(int iNo, char Ch)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    for(iCnt = 1; iCnt <= iNo; iCnt++)   //N
+    {
+        printf("%c\t",Ch);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+    char cValue = '\0';
+
+    printf("Enter frequency : \n");
+    scanf("%d",&iValue);
+
+    printf("Enter character : \n");
+    scanf("%c",&cValue);        //not accepting character
+
+    printf("Accepted character is : %c\n",cValue);
+
+    Display(iValue,cValue);
+
+    return 0;
+}
+
+// Time Complexity O(N)
+// N is a natural number
+//Where N >= 0 and N <= Infinity
---
+++ Pattern Printing/87_PatternSequence.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+//sequence
+void Display()
+{
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+
+    printf("\n");
+
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+
+    printf("\n");
+
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+
+    printf("\n");
+}
+
+int main()
+{       
+    Display();
+
+    return 0;
+}
